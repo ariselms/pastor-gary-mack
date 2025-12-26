9ac9559295789012843cc604fa9d3638716e7465
Reynaldo Navedo <rnavedo@ariselms.com>
2025-12-25 23:25:02 -0500

    changes to middleware to address stripe webhook issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const config = {
+  matcher: [
+    // Protege todo EXCEPTO:
+    // - assets
+    // - stripe webhooks
+    "/((?!_next/static|_next/image|favicon.ico|api/webhooks).*)"
+  ]
+};
+
 export function middleware(request: NextRequest) {
 	// Tu lógica normal de auth aquí
 	return NextResponse.next();
 }
 
-export const config = {
-	matcher: [
-		// Protege todo EXCEPTO:
-		// - assets
-		// - stripe webhooks
-		"/((?!_next/static|_next/image|favicon.ico|api/webhooks).*)"
-	]
-};
