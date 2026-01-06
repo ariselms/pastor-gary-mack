9137e35cfb7b4d015b02ad7a2f12e0341aa79e64
Reynaldo Navedo <rnavedo@ariselms.com>
2026-01-06 12:05:59 -0500

    minor changes to ebook reader colors

--- public/images/prod_TehIIkDgPttgf7_files/src_app_layout_tsx_f8182b1e._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__76810193._.js",
-  "static/chunks/node_modules_d5bd39eb._.js",
-  "static/chunks/[root-of-the-server]__7bd05aa3._.css"
-],
-    source: "dynamic"
-});
--- public/images/prod_TehIIkDgPttgf7_files/src_app_profile_page_tsx_4a59e23b._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9699e73b._.js"
-],
-    source: "dynamic"
-});
